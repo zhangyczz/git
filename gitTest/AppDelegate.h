6b70fb135d3f2f4fbaa49305baea2dc819d8bade
郭天翔 <guotx@5173.com>
2018-03-27 16:04:58 +0800

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by jhlc5173 on 2018/3/27.
+//  Copyright © 2018年 jhlc5173. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by jhlc5173 on 2018/3/27.
+//  Copyright © 2018年 jhlc5173. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
